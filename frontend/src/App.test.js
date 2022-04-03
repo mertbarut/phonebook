658d8bbae3c6b292fe41ecec4280d6c336adfa86
mertbarut <mbarut@icloud.com>
2022-04-03 02:12:52 +0200

    completed part4a

---
+++ frontend/src/App.test.js
@@ -0,0 +1,5 @@
+describe('empty app test', () => {
+  test('test1', () => {
+    expect(1).toBe(1)
+  })
+})
---
+++ utils/list_helper.js
@@ -0,0 +1,66 @@
+var _ = require('lodash')
+
+const dummy = (blogs) => {
+	return 1
+}
+
+const totalLikes = (blogs) => {
+	return blogs.map(blog => blog.likes).reduce((prev, next) => prev + next)
+}
+
+const favoriteBlog = (blogs) => {
+	const likes = blogs.map(blog => blog.likes)
+	//console.log(likes)
+	const max = Math.max(...likes)
+	//console.log(max)
+	const index = likes.indexOf(max)
+	//console.log(index)
+
+	return blogs[index]
+}
+
+const mostBlogs = (blogs) => {
+	const blogCountByAuthor = _.countBy(blogs, 'author')
+	const blogsArray = _.entries(blogCountByAuthor)
+	const authorWithMostPosts = _.maxBy(blogsArray, _.last)
+	const author = {
+		author: String(authorWithMostPosts[0]),
+		blogs: Number(authorWithMostPosts[1])
+	}
+	return author
+}
+
+const mostLikes = (blogs) => {
+	let likesArray = []
+	const blogsByAuthor = _.groupBy(blogs, 'author')
+	const authorsArray = _.entries(blogsByAuthor)
+	//console.log(authorsArray)
+	_.forEach(authorsArray, (author) => {
+		//console.log(totalLikes(author[1]))
+		likesArray.push({
+			author: author[0],
+			likes: totalLikes(author[1])
+		})
+	})
+	//console.log(likesArray)
+	const likes = likesArray.map(blog => blog.likes)
+	//console.log(likes)
+	const max = Math.max(...likes)
+	//console.log(max)
+	const index = likes.indexOf(max)
+	//console.log(index)
+
+	const author = {
+		author: String(likesArray[index].author),
+		likes: Number(likesArray[index].likes)
+	}
+	return author
+}
+
+module.exports = {
+	dummy,
+	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+	mostLikes
+}
