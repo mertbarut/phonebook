eb879a0720cd4bf925c8069a15b2efdf7436523b
mertbarut <mbarut@icloud.com>
2022-04-11 01:09:38 +0200

    redux

---
+++ frontend/src/components/Counter.js
@@ -0,0 +1,26 @@
+const Counter = ({ store }) => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button 
+        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'DECREMENT' })}
+      >
+        minus
+      </button>
+      <button 
+        onClick={e => store.dispatch({ type: 'ZERO' })}
+      >
+        zero
+      </button>
+    </div>
+  )
+}
+
+export default Counter
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,34 @@
 import ReactDOM from 'react-dom'
-import App from './App'
+import AppBlogs from './App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+import { createStore } from 'redux'
+
+import Counter from './components/Counter'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+const renderBlogs = () => {
+  ReactDOM.render(<AppBlogs />, document.getElementById('root'))
+}
+
+const renderCounter = () => {
+  ReactDOM.render(<Counter store={store}/>, document.getElementById('root'))
+}
+
+//renderBlogs()
+renderCounter()
+
+store.subscribe(renderCounter)
