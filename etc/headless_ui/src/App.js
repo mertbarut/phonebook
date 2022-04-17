d8325c23067eb061298f88f4de698b06ab150f64
mertbarut <mbarut@icloud.com>
2022-04-18 00:54:08 +0200

    headlessUI

--- etc/headless_ui/src/App.js
+++ etc/headless_ui/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
-function App() {
+import Dropdown from './components/DropdownMenu';
+import ListboxSelect from './components/ListboxSelect';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex justify-center items-center min-h-screen bg-indigo-500">
+      <div class="flex-5 grow-0 shrink-0 max-w-5x1 p-20">
+        <div className="grid grid-cols-1 grid-rows-5 gap-20 grid-flow-col lg:grid-cols-1">
+          <div class="p-20 min-w-min bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2">
+            <Dropdown />
+          </div>
+          <div class="p-20 pr-10 min-w-min bg-white border-1-8 border-transparent rounded-md shadow-md space-y-2">
+            <ListboxSelect />
+          </div>
+        </div>
+      </div>
     </div>
+
+
   );
 }
 
